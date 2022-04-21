ed893f1da6ef0f887035bfc60e681e4c9215f7e8
Aryanne Acosta <aryanneacosta@gmail.com>
2022-04-21 20:08:55 -0300

    layout quiz pelo JS

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const URL_API = "https://mock-api.driven.com.br/api/v4/buzzquizz";
+const arrQuizzes = [];
+const idQuizzes = [];
+let quizzTitle;
 
 function pegarQuizzes() {
     const promise = axios.get(`${URL_API}/quizzes`);
@@ -10,14 +13,15 @@ function renderizarQuizzes(resposta) {
     console.log(resposta.data);
     const containerQuizz = document.querySelector(".containerImagens");
     for (let i = 0; i < resposta.data.length; i++) {
-        console.log(resposta.data[i].title);
+        idQuizzes.push(resposta.data[i].id); 
+        arrQuizzes.push(resposta.data[i].title);
         containerQuizz.innerHTML += `
             <div class="top2" onclick="acessarQuizz(this)"> 
-                <img src="${resposta.data[i].image}" class="imagemQuizz"> 
-            <div class="tituloQuizz" > 
-                ${resposta.data[i].title} 
-            </div>
-        </div> `
+                    <img src="${resposta.data[i].image}" class="imagemQuizz"> 
+                <div class="tituloQuizz" > 
+                    ${resposta.data[i].title} 
+                </div>
+            </div> `
     }
 }
 
@@ -27,19 +31,95 @@ function erroPegarQuizzes() {
     alert("Erro na coleta de dados da API. Tente novamente.");
 }
 
-function acessarQuizz(idQuizz) {
-    let promise = axios.get(`${URL_API}/quizzes/${idQuizz}`);
+function acessarQuizz(quizzTitle) {
+    
+    const tituloQuizz = quizzTitle.querySelector(".tituloQuizz").textContent;
+    console.log(tituloQuizz);
+    let idQuizz; 
+    let i = 0;
+
+    while (i < arrQuizzes.length) {
+
+        if (arrQuizzes[i] === tituloQuizz) {
+            
+            idQuizz = idQuizzes[i];
+            
+        }
+        i++;
+    }
+    
+    
+    let promise = axios.get(`${URL_API}/quizzes/8023`);
     promise.then(mostrarTelaQuizz);
     promise.catch(erroAcessarQuizz);
 }
 
-function mostrarTelaQuizz() {
+function mostrarTelaQuizz(perguntas) {
+    
     document.querySelector(".telaUm").classList.add("escondido");
     document.querySelector(".telaDois").classList.remove("escondido");
+
+    //html do quizz
+    const paginaQuizz = document.querySelector(".perguntas");
+    paginaQuizz.innerHTML =  
+    `<div class="página quizz">
+        <div class="top">
+            <img src="${perguntas.image}" />
+            <div class="titulo-quizz">${perguntas.title}</div>
+        </div>
     
+        <div class="conteudo-perguntas">
+        </div>
+    </div>`;
+
+    const perguntaQuizz = document.querySelector(".conteudo-perguntas");
+    for (let i = 0; i < perguntas.questions.length; i++ ) {
+        perguntaQuizz.innerHTML += `
+        <div class="perguntas">
+
+        <div class="pergunta">${perguntas.questions.title}</div>
+        <div class="respostas">`
+
+        for (let i = 0; i < perguntas.questions.answers.length; i++ ) {
+            `<div class="resposta">
+                <img src="${perguntas.questions.answers.image}"/>
+                <div class="resposta-texto">${perguntas.questions.answers.text}</div>         
+            </div>`
+        }   
+        `</div>
+        </div>`
+    }
+    
+
 }
 
 function erroAcessarQuizz(erro) {
     alert("Erro ao acessar quizz. Tente novamente.");
     console.log(resposta.data.erro);
 }
+
+`
+
+    
+
+
+
+    <div class="pergunta">Qual o sobrenome do Harry?</div>
+    <div class="respostas">
+        <div class="resposta">
+            <img src="https://gkpb.com.br/wp-content/uploads/2021/12/gkpb-cinemark-reexibe-harry-potter.jpg"/>
+            <div class="resposta-texto">Styles</div>        
+        </div>
+        <div class="resposta">
+            <img src="https://gkpb.com.br/wp-content/uploads/2021/12/gkpb-cinemark-reexibe-harry-potter.jpg"/>
+            <div class="resposta-texto">Styles</div>
+        </div>
+        <div class="resposta">
+            <img src="https://gkpb.com.br/wp-content/uploads/2021/12/gkpb-cinemark-reexibe-harry-potter.jpg"/>
+            <div class="resposta-texto">Styles</div>
+        </div>
+        <div class="resposta">
+            <img src="https://gkpb.com.br/wp-content/uploads/2021/12/gkpb-cinemark-reexibe-harry-potter.jpg"/>
+            <div class="resposta-texto">Styles</div>
+        </div>
+    </div>`
